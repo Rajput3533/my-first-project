9fac446f60a9a213f6d47e9f85bfdd2264f69ccb
Abhishek Singh <abhishekrajput2697909@gmail.com>
2026-04-10 19:36:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,33 @@ function add() {
     let b = document.getElementById("num2").value;
 
     let sum = Number(a) + Number(b);
-
     document.getElementById("result").innerText = "Result: " + sum;
+}
+
+function subtract() {
+    let a = document.getElementById("num1").value;
+    let b = document.getElementById("num2").value;
+
+    let result = Number(a) - Number(b);
+    document.getElementById("result").innerText = "Result: " + result;
+}
+
+function multiply() {
+    let a = document.getElementById("num1").value;
+    let b = document.getElementById("num2").value;
+
+    let result = Number(a) * Number(b);
+    document.getElementById("result").innerText = "Result: " + result;
+}
+
+function divide() {
+    let a = document.getElementById("num1").value;
+    let b = document.getElementById("num2").value;
+
+    if (b == 0) {
+        document.getElementById("result").innerText = "Cannot divide by 0";
+    } else {
+        let result = Number(a) / Number(b);
+        document.getElementById("result").innerText = "Result: " + result;
+    }
 }
